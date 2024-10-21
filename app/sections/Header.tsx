de5701acb65837c5d6187b6c844a131f0a99dd7f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-10-21 17:58:35 +0300

    some improvements

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -15,20 +15,20 @@ const isBrowser = () => typeof window !== 'undefined'; //The approach recommende
 
 const Header = () => {
   return (
-    <header id="header" className="mb-10 sticky top-0 backdrop-blur-sm z-20">
+    <header  className="mb-10 sticky top-0 backdrop-blur-sm z-20">
       <div className="container sm:mx-auto md:max-auto lg:mx-auto">
         <div className="flex items-center justify-between sm:justify-between lg:justify-around md:justify-around 2xl:justify-around">
           <div className="flex items-center gap-2 px-4 py-4">
             <Image className="lg:w-10" src={blacklogo} alt="logo"></Image>
             <h1 onClick={scrollToTop} className="font-extrabold tracking-tighter bg-gradient-to-b from-black to-[#515253] text-transparent bg-clip-text sm:text-xl md:text-2xl lg:text-3xl cursor-pointer">
                 İsmail Emir
-              </h1>
+            </h1>
           </div>
-          <div className="px-4 py-4 sm:hidden">
+          <div className="px-4 py-4 sm:hidden ml-auto">
             <Image src={headerMenu} alt="header menu"></Image>
           </div>
           <div className="hidden sm:block ">
-            <ul className="flex items-center gap-5 font-semibold lg:text-2xl">
+            <ul className="flex items-center gap-5 font-semibold sm:text-sm lg:text-2xl">
               <Link to="about" smooth={true} duration={500} offset={-150}>
                 <li className="text-orange-500 cursor-pointer transition duration-300 ease-in-out hover:text-blue-300">About Me</li>
               </Link>
